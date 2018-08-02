0223b74272a5bf86846c110a31957a5de6fc8991
Andy <andrex.lee@gmail.com>
2018-08-02 13:34:46 -0400

    added notes to functions

--- softeners.py
+++ softeners.py
@@ -1,13 +1,14 @@
 import pandas as pd
 
 
-
+# Loads water quality data
 data = pd.read_csv('./hardness.csv')
 data.set_index('zipcode', inplace = True)
 print(data)
 
 # location != data.loc[int(location)]
 
+# returns the water hardness for the user/'s zipcode
 def get_location():
     location = input('What is you zip code?: ')
     if int(location) <= 0 or int(location) >= 99999:
@@ -21,18 +22,19 @@ sample = str(input('Do you have a water sample? yes or no '))
 num_people = int(input('How many people live in your home? '))
 fixtures = str(input('Do you have any high flow fixtures? '))
 
+# If the user has a sample we ask for it here, if not we get the hardness from our data
 if sample.lower() == 'yes':
     hardness = int(input('What is your hardness amount? '))
     iron = int(input('what are your iron results? '))	
 else:
     hardness = get_location()
 
-
+# We determine if the user has city or well water
 if water_type.lower() != 'city' or 'well':
     print('Please enter either well or city for water type')
     water_type = input('What type of water do you have, city or well? ')
     
-
+# We calculate the size of softener needed based on hard
 if water_type.lower() == 'city':
     hardness_calc = (75 * num_people * hardness)
 elif water_type.lower() == 'well':
